2dbc80e5c2badde90723fb684be7f3a821e83d4b
shivc868 <shivc8658@gmail.com>
2022-04-07 16:20:44 +0530

    first commit

---
+++ src/views/ServicesView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus,
+      aut ea! Ea sunt unde sapiente fugiat suscipit voluptas aliquam est hic
+      inventore provident vel eveniet, corrupti praesentium doloremque corporis
+      sint.
+    </h1>
+  </main>
+</template>
